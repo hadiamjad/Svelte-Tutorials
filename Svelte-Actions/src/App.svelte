b0a5cb5dd59b78034160f4d5b209eff67db2dc5d
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-08 23:42:46 +0530

    Add files via upload

---
+++ Svelte-Actions/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+    let show = true;
+
+    function showAndHideNode(node) {
+        node.setAttribute("style", "color: red;")
+    }
+</script>
+
+<main>
+
+    <input type="checkbox" bind:checked={show}>
+
+    {#if show}
+        <p id="svelte-paragraph" class="svelte-class" use:showAndHideNode> Svelte is cool to go with!!! </p>
+    {:else }
+        <p>
+            <strong class="svelte-class" > Svelte element Destroyed </strong>
+        </p>
+
+    {/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
